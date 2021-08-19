4fbc35b0eba9df704ae79eb3a3325f0871650de0
nao-18 <kyahyo17@yahoo.co.jp>
2021-08-20 02:59:51 +0900

    null-and-undefined

---
+++ src/null-and-undefined.ts
@@ -0,0 +1,12 @@
+export {}
+
+//nullのみ許容
+let absence: null = null;
+// absence = 123;
+
+console.log({absence});
+
+//undefinedのみ許容
+let data: undefined = undefined;
+// data = 123;
+console.log({undefined});
