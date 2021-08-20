ad613e6e1f30933237b13eb03af94f8c18a9b9c9
nao-18 <kyahyo17@yahoo.co.jp>
2021-08-21 07:47:07 +0900

    anonymouse-function

---
+++ src/anonymouse-function.ts
@@ -0,0 +1,16 @@
+export {};
+
+/**
+ * 無名関数の型
+ * @param height
+ * @param weight
+ * @returns
+ */
+let bmi: (height: number, weight: number) => number = function (
+  height: number,
+  weight: number
+): number {
+  return weight / (height * weight);
+};
+
+console.log(bmi(1.78, 80));
