b127b1240e9d78f2c5d5a6acea34ef135ae4b43e
nao-18 <kyahyo17@yahoo.co.jp>
2021-08-21 08:32:13 +0900

    default-parameters

---
+++ src/default-parameters.ts
@@ -0,0 +1,13 @@
+export {};
+
+/**
+ * デフォルトのパラメーター値
+ * @param currentSalary
+ * @param rate
+ * @returns
+ */
+const nextYearSalary = (currentSalary: number, rate: number = 1.1): number => {
+  return currentSalary * rate;
+};
+
+console.log(nextYearSalary(1000));
