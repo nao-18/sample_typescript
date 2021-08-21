7d35c5149830427a3a2ef050c56741ca4adccb8d
nao-18 <kyahyo17@yahoo.co.jp>
2021-08-21 09:22:31 +0900

    overloads

---
+++ src/overloads.ts
@@ -0,0 +1,22 @@
+export {};
+
+/**
+ * オーバーライド
+ */
+
+//シグネチャー
+//関数やメソッドの概略を記述したもの
+function double(value: number): number;
+function double(value: string): string;
+
+function double(value: any): any {
+  console.log(typeof value);
+  if (typeof value === 'number') {
+    return value * 2;
+  } else {
+    return value + value;
+  }
+}
+
+console.log(double(100));
+console.log(double('Go '));
